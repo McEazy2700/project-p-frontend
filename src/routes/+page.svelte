ab2e1498b16a86568f3d5dbeeb75cf7f409d4ac8
Ezekiel Victor <codeepoch@gmail.com>
2024-03-12 00:57:33 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<main class="w-screen overflow-hidden z-0 relative">
+	<div class="w-full absolute -z-10">
+		<img
+			class="w-full max-h-screen object-cover"
+			alt="Nike"
+			src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D%204470w"
+		/>
+	</div>
+	<div class="z-10 p-10">
+		<h2>Design & high quality</h2>
+	</div>
+</main>
